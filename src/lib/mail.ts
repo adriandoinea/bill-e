34a80e3ba2293d4583380f0d50152789448c6293
Adrian Doinea <adriandoinea03@gmail.com>
2024-10-05 01:51:18 +0300

    Fix wrong URL for password reset email

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -15,7 +15,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `https://bill-e.org/auth/new-password?token=${token}`;
   await resend.emails.send({
     from: "no-reply@bill-e.org",
     to: email,
